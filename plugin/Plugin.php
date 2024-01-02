1021b9f10cf912cc6e5e7acef6cfc29d4c94e09e
imkero <kerorek@outlook.com>
2024-01-02 16:11:38 +0800

    feat: variable support

--- plugin/Plugin.php
+++ plugin/Plugin.php
@@ -4,8 +4,10 @@ namespace TypechoPlugin\CustomHeaderFooter;
 use Typecho\Plugin\PluginInterface;
 use Typecho\Widget\Helper\Form;
 use Typecho\Widget\Helper\Layout;
+use Typecho\Widget\Helper\Form\Element;
 use Typecho\Widget\Helper\Form\Element\Textarea;
 use Widget\Options;
+use Widget\User;
 
 if (!defined('__TYPECHO_ROOT_DIR__')) {
     exit;
@@ -44,6 +46,9 @@ class Plugin implements PluginInterface
      */
     public static function config(Form $form)
     {
+        $description = new PluginDescription('description', NULL, NULL, '说明', NULL);
+        $form->addInput($description);
+
         $headHtml = new MonospaceTextarea('headHtml', null, '', '&lt;head&gt; 标签内自定义 HTML');
         $form->addInput($headHtml);
 
@@ -65,7 +70,13 @@ class Plugin implements PluginInterface
      */
     public static function renderHeadHtml()
     {
-        echo Options::alloc()->plugin('CustomHeaderFooter')->headHtml;
+        $variables = self::collectVariables();
+        $html = Options::alloc()->plugin('CustomHeaderFooter')->headHtml;
+        foreach ($variables as $key => $value) {
+            // 变量格式: {VAR_NAME}
+            $html = str_replace("{{$key}}", $value, $html);
+        }
+        echo $html;
     }
 
     /**
@@ -73,8 +84,31 @@ class Plugin implements PluginInterface
      */
     public static function renderFooterHtml()
     {
-        echo Options::alloc()->plugin('CustomHeaderFooter')->footerHtml;
+        $variables = self::collectVariables();
+        $html = Options::alloc()->plugin('CustomHeaderFooter')->footerHtml;
+        foreach ($variables as $key => $value) {
+            // 变量格式: {VAR_NAME}
+            $html = str_replace("{{$key}}", $value, $html);
+        }
+        echo $html;
     }
+
+    public static function collectVariables() {
+        User::alloc()->to($user);
+        Options::alloc()->to($options);
+
+        return [
+            'SITE_URL' => $options->siteUrl,
+            'THEME_URL' => $options->themeUrl,
+            'USER_GROUP' => $user->group ?? 'null',
+        ];
+    }
+
+    public const VARIABLE_DESCRIPTION = [
+        'SITE_URL' => '站点 URL',
+        'THEME_URL' => '主题目录 URL',
+        'USER_GROUP' => '当前用户的用户组 (administrator, editor, contributor, subscriber, visitor, null)',
+    ];
 }
 
 /**
@@ -93,5 +127,54 @@ class MonospaceTextarea extends Textarea {
         $layout = parent::input($name, $options);
         $layout->style = 'font-family: monospace';
         return $layout;
+    }
+}
+
+/**
+ * 插件设置页面头部说明
+ */
+class PluginDescription extends Element
+{
+    /**
+     * 初始化当前输入项
+     *
+     * @param string|null $name 表单元素名称
+     * @param array|null $options 选择项
+     * @return Layout|null
+     */
+    public function input(?string $name = null, ?array $options = null): ?Layout
+    {
+        $p1 = new Layout('p');
+        $p1->html('本插件支持在 &lt;head&gt; 标签内和 &lt;/body&gt; 标签之前插入自定义 HTML (对应主题代码中的 header() 和 footer() 调用)');
+        $this->container($p1);
+
+        $p2 = new Layout('p');
+        $p2->html('在 HTML 代码中支持代入以下变量:');
+        $this->container($p2);
+
+        $ul = new Layout('ul');
+        foreach (Plugin::collectVariables() as $key => $value) {
+            $key = htmlspecialchars($key);
+            $value = htmlspecialchars($value);
+            $varDesc = htmlspecialchars(Plugin::VARIABLE_DESCRIPTION[$key]);
+
+            $li = new Layout('li');
+            $li->html("<p><code>{{$key}}</code>: <code>{$value}</code><br><span style=\"color:#999;font-size:.9em\">{$varDesc}</span>");
+            $ul->addItem($li);
+        }
+        $this->container($ul);
+        
+        $input = new Layout('input');
+        return $input;
+    }
+
+    /**
+     * 设置表单项默认值
+     *
+     * @param mixed $value 表单项默认值
+     */
+    protected function inputValue($value)
+    {
+        $this->input->html($value);
     }
 }
